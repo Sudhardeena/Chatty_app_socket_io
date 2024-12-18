cb8bba590cf20554cd73c26ddd9edd3626c5871a
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-18 20:06:50 +0530

    with basic auth routes and controllers setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Chatty app");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
   console.log("server listening at port 5001");
